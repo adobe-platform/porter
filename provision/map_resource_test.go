93594f987c3c89b87aef3072332b28a32b69f1a5
Brandon Cook <bcook@adobe.com>
2016-06-14 09:44:26 -0700

    initial test suite

---
+++ provision/map_resource_test.go
@@ -0,0 +1,16 @@
+package provision_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	_ "github.com/adobe-platform/porter/provision"
+)
+
+var _ = Describe("Map resources", func() {
+
+	It("runs", func() {
+		Expect(nil).To(BeNil())
+	})
+
+})
---
+++ provision/suite_test.go
@@ -0,0 +1,13 @@
+package provision_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCli(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Provision Suite")
+}
